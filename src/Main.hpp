402644f1a2ca2fc484c5fc0239263e82240ad25c
Camden Smallwood <camden.smallwood@gmail.com>
2015-09-18 13:03:31 -0400

    Added platform header, some cleanup.
    Added a platform header for setting compiler-specific definitions.
    Performed a small amount of code cleanup.

--- src/Main.hpp
+++ src/Main.hpp
@@ -1,8 +1,11 @@
-#ifndef MAIN_HPP
-#define MAIN_HPP
+#ifndef DARKCORE_MAIN_HPP
+#define DARKCORE_MAIN_HPP
+
+#include "Platform.hpp"
 
 #define Hook(func1, func2) o##func1 = (t##func1)GetProcAddress(hMod, #func2)
 #define DetourHook(func) o##func = (t##func)DetourAttach(&(PVOID&)GetProcAddress(hMod, #func), hk##func)
 #define DetourUnhook(func) DetourDetach(&(PVOID&)o##func, hk##func);
 #define DllExport extern "C" __declspec(dllexport)
-#endif
+
+#endif /* DARKCORE_MAIN_HPP */
