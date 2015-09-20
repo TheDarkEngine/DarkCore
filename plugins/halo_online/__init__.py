665eae58b442beb644926a91650651f394dc9626
Camden Smallwood <camden.smallwood@gmail.com>
2015-09-20 15:48:32 -0400

    Changed around example 'halo_online' module.

--- plugins/halo_online/__init__.py
+++ plugins/halo_online/__init__.py
@@ -14,38 +14,40 @@ import darkcore
 #       It will be populated on the C++ side of darkcore,
 #       after parsing the JSON into Python values.
 __info__ = {
-	"patches": [{
-		"title": "Hud Hide",
-		"author": "DARKC0DE",
-		"description": "This patch will allow you to hide the HUD",
-		"pattern": "F0 3F D9 5D 93 A8 E2 DD 83 3F 00",
-		"match": "xxxxxxxxxxx",
-		"offset": 6,
-		"patch": "C3 F5 48 40",
-		"recursivePatch": True,
-		"patchOnStartup": False,
-		"patchBeforeStartup": False
-	}, {
-		"title": "Hud Show",
-		"author": "DARKC0DE",
-		"description": "This patch will allow you to show the HUD",
-		"pattern": "F0 3F D9 5D 93 A8 C3 F5 48 40 00",
-		"match": "xxxxxxxxxxx",
-		"offset": 6,
-		"patch": "E2 DD 83 3F",
-		"recursivePatch": True,
-		"patchOnStartup": False,
-		"patchBeforeStartup": False
-	}],
-	"hooks": [{
-		"title": "Player Death Hook",
-		"author": "DARKC0DE",
-		"description": "This pattern finds the Player Death Hook",
-		"pattern": "00 00 00 00 00 00 00",
-		"match": "xxxxxxx",
-		"offset": 0,
-		"hookType": "pre"
-	}],
+	"patches": {
+		"Hude Hide": {
+			"author": "DARKC0DE",
+			"description": "This patch will allow you to hide the HUD",
+			"pattern": "F0 3F D9 5D 93 A8 E2 DD 83 3F 00",
+			"match": "xxxxxxxxxxx",
+			"offset": 6,
+			"patch": "C3 F5 48 40",
+			"recursive": True,
+			"patchOnStartup": False,
+			"patchBeforeStartup": False
+		},
+		"Hud Show": {
+			"author": "DARKC0DE",
+			"description": "This patch will allow you to show the HUD",
+			"pattern": "F0 3F D9 5D 93 A8 C3 F5 48 40 00",
+			"match": "xxxxxxxxxxx",
+			"offset": 6,
+			"patch": "E2 DD 83 3F",
+			"recursive": True,
+			"patchOnStartup": False,
+			"patchBeforeStartup": False
+		}
+	},
+	"hooks": {
+		"Player Death": {
+			"author": "DARKC0DE",
+			"description": "This pattern finds the Player Death Hook",
+			"pattern": "00 00 00 00 00 00 00",
+			"match": "xxxxxxx",
+			"offset": 0,
+			"type": "pre"
+		}
+	},
 	"title": "DarkPlugin Test",
 	"description": "This plugin demonstrates hooking and patching",
 	"author": "DARKC0DE",
@@ -56,7 +58,7 @@ __info__ = {
 # This is a reserved function that gets called when the plugin is loaded 
 def on_plugin_start():
 	darkcore.debug_log("Starting " + __info__["title"])
-	darkcore.hook_event("Player Death Hook", on_player_death)
+	darkcore.hook_event("Player Death", on_player_death)
 
 # This is a reserved function that gets called when the plugin is unloaded 
 def on_plugin_exit():
@@ -67,32 +69,9 @@ def on_player_death(**kwargs):
 	# TODO: Determine which player died
 	darkcore.debug_log("Player died.")
 
+#
 # TODO: Think of a good way to structure custom events...
+#
 
-# this is a custom command, see DarkEventHandler 
-def MyCommandTest():
-    Logging.Debug.WriteToLog("MyCommandTest")
-
-"""
-This is a reserved
-
-function that gets called when our plugin gets an event We will pass events directly to plugins using the WebSocket API This is totally optional to include in a plugin and is only needed
-if events need to be called from the WebSocket API
-
-An example an event could be 
-{
-    "command": "Run Patch",
-    "patch": "Hide Hud"
-}
-
-This event will get ran regardless
-if the DarkEventHandler exists.If DarkEventHandler, you can prevent the previous event from happening and do something
-else instead
-
-"""
-def DarkEventHandler(e):
-    Logging.Debug.WriteToLog("Hit DarkEventHandler(): " + e) 
-    if e.command == "MyCommandTest":
-    	MyCommandTest()
-
-print("Woo!")
+# TODO: Call 'on_plugin_start' from DarkCore
+on_plugin_start()
